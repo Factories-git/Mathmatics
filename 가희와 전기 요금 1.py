dfaa40bce9636712f0b79f0190ba918047de5a2f
Factories-git <official.factories@gmail.com>
2025-02-25 09:56:40 +0900

    Changes

--- 가희와 전기 요금 1.py
+++ 가희와 전기 요금 1.py
@@ -1,6 +1,4 @@
-for a in range(100,1000):
-    for m in range(100, 10**5):
-        # for i in range(int(input())):
-        #     # a, m = map(int, input().split())
-        w_m = m / 60
-        print(round((int(a*w_m)) * 105.6 // 1000))
+q = int(input())
+for i in range(q):
+    a, m = map(int, input().split())
+    print(int((105.6 * (a*m)) // 60000))
