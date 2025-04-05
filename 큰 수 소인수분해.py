8989ad546265ef2548b0301c7a86a8fdd91472d6
Factories-git <official.factories@gmail.com>
2025-04-05 15:02:45 +0900

    Changes

---
+++ 큰 수 소인수분해.py
@@ -0,0 +1,78 @@
+from math import gcd
+import random
+
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+
+def isprime(p):
+    if p == 2 or p == 3:
+        return True
+    if p < 2 or p % 2 == 0:
+        return False
+
+    r = 0
+    d = p-1
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+
+    for a in primes:
+        if a >= p:
+            continue
+        x = pow(a, d, p)
+
+        if x == 1 or x == n-1:
+            continue
+
+        for i in range(r-1):
+            x = pow(x, 2, p)
+
+            if x == p-1:
+                break
+        else:
+            return False
+
+    return True
+
+
+def pollard_rho(n):
+    if isprime(n):
+        return n
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+    if n % 5 == 0:
+        return 5
+    while True:
+        x = random.randrange(2, n)
+        y = x
+        c = random.randrange(2, n)
+        f = lambda x: (pow(x, 2, n) + c) % n
+
+        d = 1
+
+        while d == 1:
+            x = f(x)
+            y = f(f(y))
+            d = gcd(abs(x - y), n)
+        if d != n:
+            return d
+
+
+n = int(input())
+if n == 1:
+    print(1)
+factor = []
+re = 1
+while n != 1:
+    re = pollard_rho(n)
+    while n % re == 0:
+        factor.append(re)
+        n //= re
+
+for i in sorted(factor):
+    print(i)
